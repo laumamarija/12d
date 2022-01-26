b1eee548a573063f19fa5949a92ba43b83b3fae0
Lauma Puksta <98461081+laumamarija@users.noreply.github.com>
2022-01-26 12:26:21 +0000

    4 uzdevuma izpilde

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+#import math
+#x=int(input("ievadi kvadrata malas garumu, kas ir lielaka par 5cm: "))
+#kvadrataS=math.pow(x,2)
+#y=x+5
+#kvadrata2S=math.pow(y,2)
+#z=(kvadrata2S-kvadrataS)/kvadrataS*100
+#print(round(z),"%")
+
 import math
-x=int(input("ievadi kvadrata malas garumu, kas ir lielaka par 5cm: "))
-kvadrataS=math.pow(x,2)
-y=x+5
-kvadrata2S=math.pow(y,2)
-z=(kvadrata2S-kvadrataS)/kvadrataS*100
-print(round(z),"%")
+x=int(input("ievadi riņķa līnijas radiusu: "))
+y=int(input("ievadi vienādsānu taisnleņķa trijstūra hipotenūzas garumu: "))
+rinkaS=math.pi*math.pow(x,2)
+trissturaS=y*(y/2)/2
+liel=rinkaS-trissturaS
+print("Ŗiņķa laukims ir lielāks par ", round(liel, 1),"cm^3 neka trisstura laukums")
+
